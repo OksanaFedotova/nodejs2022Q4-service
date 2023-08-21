a098b12012efccdd68d99f637abc9a000600c5dd
OksanaFedotova <oxana-fedotova@yandex.ru>
2023-08-21 15:37:03 +0300

    feat: custom Exception Filter is implemented

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { CustomLogger } from './logger/logger.service';
+import { AllExceptionsFilter } from './filters/exeption.filter';
+import { HttpAdapterHost } from '@nestjs/core';
 //import { getDocs } from './utils';
 
 const PORT: number = Number(process.env.PORT) || 4000;
@@ -10,6 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: false, bufferLogs: true });
 
   app.useLogger(app.get(CustomLogger));
+  const adapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(adapterHost));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
